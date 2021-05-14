7c9f1cb40ba597736e0d264d96cb479522fceec2
wndyy <30420239+wndyy@users.noreply.github.com>
2021-05-13 23:44:56 -0700

    Update twoSum.js

--- twoSum.js
+++ twoSum.js
@@ -1,11 +1,15 @@
 var twoSum = function(nums, target) {
     let i;
     let j;
+    let hashMap = {}
+    let sub;
     for (i = 0; i < nums.length; i++) {
-        for (j = 0; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target && i != j) {
-                return [i, j]
-            }
+        hashMap[nums[i]] = i;
+    }
+    for (j = 0; j < nums.length; j++) {
+        sub = target - nums[j]
+        if (hashMap[sub] && hashMap[sub] != j) {
+            return [hashMap[sub], j]
         }
     }
 };
